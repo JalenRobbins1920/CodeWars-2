2d5cef9cacd883c50ef1fa74a1849075e3801b6f
JalenRobbins1920 <jgrobbibs10@gmail.com>
2025-09-20 22:46:37 -0400

    Add files via upload

---
+++ Convery_integer_currency.py
@@ -0,0 +1,5 @@
+def toCurrency(number):
+    return "{:,}".format(number)
+
+print(toCurrency(385000))
+print(toCurrency(18500000))
---
+++ OddorEven.py
@@ -0,0 +1,14 @@
+def oddOrEven(arr):
+    total = sum(arr)
+    if total % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
+
+    
+arr = [2,4,6]
+print(oddOrEven(arr))
+
+#https://www.codewars.com/kata/5949481f86420f59480000e7
+#Kata name Odd or Even
